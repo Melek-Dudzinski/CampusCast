2727b7e7dcf65e1aaa728dfc6a22b190d85cf9ca
ec221148 <ec221148@qmul.ac.uk>
2024-03-12 17:02:06 +0000

    Added components for main

---
+++ campuscast/app/main/navbar.js
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import './navbar.css'
+
+export default function Navbar() {
+    return (
+        <>
+            <header>
+                <h1 id="greeting">Hello User!</h1>
+            </header>
+            <nav>
+                <Image src="/personal.png" width="100" height="100" alt="Personal"/>
+                <Image src="/university1.png" width="100" height="100" alt="University"/>
+                <Image src="/house.png" width="100" height="100" alt="Home"/>
+                <Image src="/search.png" width="100" height="100" alt="Search"/>
+            </nav>
+        </>
+    )
+}
