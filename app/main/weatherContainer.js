d1c8d75cd65879cd7ed5c566dbb7f66eb5b9370e
Mustafa Mahmoud El Gawish Acedera <ec22746@qmul.ac.uk>
2024-03-13 14:12:21 +0000

    Added dummy text for weather

--- app/main/weatherContainer.js
+++ app/main/weatherContainer.js
@@ -9,10 +9,10 @@ export default function WeatherContainer({Title}) {
             <h3 id="titles">{Title}</h3>
             <div class="weather">
               <ul class="elements">
-                <li>Time<img src="/time.png" width="32" height="32" alt="Time symbol"/></li>
-                <li>Temperature<img src="/temperature.png" id="temp" width="32" height="32" alt="Temperature symbol"/></li>
-                <li>Wind<img src="/wind.png" id="wind" width="32" height="32" alt="Wind symbol"/></li>
-                <li>Humidity<img src="/humidity.png" id="humidity" width="32" height="32" alt="Humidity symbol"/></li>
+                <li>Time: 12:00am<img src="/time.png" width="32" height="32" alt="Time symbol"/></li>
+                <li>Temperature: 97°C<img src="/temperature.png" id="temp" width="32" height="32" alt="Temperature symbol"/></li>
+                <li>Wind: 5m/s<img src="/wind.png" id="wind" width="32" height="32" alt="Wind symbol"/></li>
+                <li>Humidity: 10%<img src="/humidity.png" id="humidity" width="32" height="32" alt="Humidity symbol"/></li>
               </ul>
             </div>
           </div>
