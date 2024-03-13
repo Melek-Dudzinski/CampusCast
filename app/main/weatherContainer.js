c2d58f5f7e7fc8559d8dcddf3935ea134d96e312
Mustafa Mahmoud El Gawish Acedera <ec22746@qmul.ac.uk>
2024-03-13 14:15:37 +0000

    Added weather data ids

--- app/main/weatherContainer.js
+++ app/main/weatherContainer.js
@@ -9,10 +9,18 @@ export default function WeatherContainer({Title}) {
             <h3 id="titles">{Title}</h3>
             <div class="weather">
               <ul class="elements">
-                <li>Time: 12:00am<img src="/time.png" width="32" height="32" alt="Time symbol"/></li>
-                <li>Temperature: 97°C<img src="/temperature.png" id="temp" width="32" height="32" alt="Temperature symbol"/></li>
-                <li>Wind: 5m/s<img src="/wind.png" id="wind" width="32" height="32" alt="Wind symbol"/></li>
-                <li>Humidity: 10%<img src="/humidity.png" id="humidity" width="32" height="32" alt="Humidity symbol"/></li>
+                <li id="time">
+                  Time: 12:00am<img src="/time.png" width="32" height="32" alt="Time symbol"/>
+                </li>
+                <li id="temperature">
+                  Temperature: 97°C<img src="/temperature.png" id="temp" width="32" height="32" alt="Temperature symbol"/>
+                </li>
+                <li id="wind">
+                  Wind: 5m/s<img src="/wind.png" id="wind" width="32" height="32" alt="Wind symbol"/>
+                </li>
+                <li id="humidity">
+                  Humidity: 10%<img src="/humidity.png" id="humidity" width="32" height="32" alt="Humidity symbol"/>
+                </li>
               </ul>
             </div>
           </div>
