161058b87d3b29000dcd96acdce9c4e5ee0b1d03
AlysaSyahin <alysasyaahin@gmail.com>
2024-03-13 16:30:03 +0000

    profile page

--- campuscast/app/main/navbar.js
+++ campuscast/app/main/navbar.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import './navbar.css'
 
 export default function Navbar() {
@@ -8,7 +9,9 @@ export default function Navbar() {
                 <h1 id="greeting">Hello User!</h1>
             </header>
             <nav>
-                <Image src="/personal.png" width="100" height="100" alt="Personal"/>
+                <Link href="/profile">
+                    <Image src="/personal.png" width="100" height="100" alt="Personal"/>
+                </Link>
                 <Image src="/university1.png" width="100" height="100" alt="University"/>
                 <Image src="/house.png" width="100" height="100" alt="Home"/>
                 <Image src="/search.png" width="100" height="100" alt="Search"/>
