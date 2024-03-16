d379149711584095243060abff1ee4aff32c92b4
Selin <s.djemal@se22.qmul.ac.uk>
2024-03-16 13:25:28 +0000

    Testing

--- app/main/weatherContainer.js
+++ app/main/weatherContainer.js
@@ -1,8 +1,68 @@
 import Image from 'next/image'
 import './weather.css'
 
+<<<<<<< Updated upstream
+=======
+const Weather = () => {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const [formattedTime, setFormattedTime] = useState('');
 
-export default function WeatherContainer({Title}) {
+  const fetchData = async () => {
+    try {
+      const MY_API_KEY = "a21c017ec647ccb8b29b6c603b5b4a43"
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&dt=${date}&appid=${MY_API_KEY}`
+      );
+      setWeatherData(response.data);
+      console.log(response.data); //You can see all the weather data in console log
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  //Need the date object with the variable name date exported
+  //To get current date would do: var date = new Date()
+  //To get a specific time would need to calculate how far from that time you are using variables
+
+  useEffect(() => {
+    const date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes().toString().padStart(2, "0");
+    let period = "";
+    if (hours < 12) {
+      // Set period to "AM"
+      period = "AM";
+      // If hours is 0 (midnight), set it to 12
+      if (hours === 0) {
+        hours = 12;
+      }
+    } else {
+      // Convert to 12-hour format and set period to "PM"
+      period = "PM";
+      hours = hours - 12;
+    }
+    const newFormattedTime = hours + ":" + minutes + period;
+    setFormattedTime(newFormattedTime);
+  }, []);
+
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleInputChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchData();
+  };
+};
+>>>>>>> Stashed changes
+
+export default function WeatherContainer({ Title, formattedTime }) {
     return (
         <>
           <div class="weather-box">
