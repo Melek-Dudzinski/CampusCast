408b4c693412209a24d3943325a7484db4f58989
Mustafa Mahmoud El Gawish Acedera <ec22746@qmul.ac.uk>
2024-03-16 15:55:20 +0000

    added search bar, fixed some css

--- app/home/navbar.js
+++ app/home/navbar.js
@@ -1,6 +1,6 @@
 'use client';
 import './navbar.css'
-import {useRef, useState} from 'react';
+import {useRef} from 'react';
 
 export default function Navbar() {
     const uni = useRef(null)
@@ -9,7 +9,8 @@ export default function Navbar() {
     const homeCaption = useRef(null)
     var uniToggle = true
 
-    function Enlarge(button){
+    function enlarge(button){
+        contract()
         if (button===uni){
             home.current.style.scale=null
             homeCaption.current.style.top=null
@@ -23,10 +24,10 @@ export default function Navbar() {
             uniToggle=false
         }
         button.current.style.scale="1.4"
-        Lower()
+        lower()
     }
 
-    function Lower(){
+    function lower(){
         if (uniToggle===true){
             uniCaption.current.style.position="relative"
             uniCaption.current.style.top="10px"
@@ -37,6 +38,21 @@ export default function Navbar() {
             homeCaption.current.style.top="10px"
             homeCaption.current.style.fontWeight = "bold"
         }
+    }
+
+    function expand(){
+        let s = document.getElementById('searchBar')
+        if (s.style.visibility==="visible"){
+            contract()
+        }
+        else{
+            s.style.visibility="visible";
+        }
+    }
+
+    function contract(){
+        let s = document.getElementById('searchBar')
+        s.style.visibility="hidden";
     }
 
     return (
@@ -52,18 +68,19 @@ export default function Navbar() {
                     <figcaption>Profile</figcaption>
                 </figure>
                 <figure>
-                    <input ref={uni} onClick={()=>Enlarge(uni)} id="university" type="image" src="/university.png"/>
+                    <input ref={uni} onClick={()=>enlarge(uni)} id="university" type="image" src="/university.png"/>
                     <figcaption ref={uniCaption} id="uniCaption">University</figcaption>
                 </figure>
                 <figure>
-                    <input ref={home} onClick={()=>Enlarge(home)} id="house" type="image" src="/home.png"/>
+                    <input ref={home} onClick={()=>enlarge(home)} id="house" type="image" src="/home.png"/>
                     <figcaption ref={homeCaption}>Home</figcaption>
                 </figure>
                 <figure id="fig">
-                    <input id="search" type="image" src="/search.png"/>
+                    <input onClick={()=>expand()} id="search" type="image" src="/search.png"/>
                     <figcaption>Search</figcaption>
                 </figure>               
             </nav>
+            <input id="searchBar" type="text" placeholder='Enter location'></input>
         </>
     )
 }
