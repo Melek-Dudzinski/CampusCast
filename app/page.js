f77bce0a2c33825eb1d0487159ca2cedf6c5ef0a
ec221148 <ec221148@qmul.ac.uk>
2024-03-17 17:10:22 +0000

    Updated landing page

--- app/page.js
+++ app/page.js
@@ -5,6 +5,9 @@ export default function Landing() {
     <>
     <div>
         <h1>Welcome to CampusCast</h1>
+        <p>
+          CampusCast is an ambitious student project for weather. It is a weather app for students where the weather you see is the weather that matters
+        </p>
         <button type="submit">
           <a href="login">Sign in</a>
         </button>
