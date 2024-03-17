3c73b3a18f68810e3c76d1c3c2bce182f2fea666
Mustafa Mahmoud El Gawish Acedera <ec22746@qmul.ac.uk>
2024-03-17 17:02:02 +0000

    update landing page

--- app/page.js
+++ app/page.js
@@ -1,13 +1,17 @@
-export default function Home() {
+import './landing.css'; 
+
+export default function Landing() {
   return (
     <>
-      <h1>Welcome to CampusCast</h1>
-      <button type="submit">
-        <a href="login">Sign in</a>
-      </button>
-      <button type="submit">
-        <a href="register">Register</a>
-      </button>
+    <div>
+        <h1>Welcome to CampusCast</h1>
+        <button type="submit">
+          <a href="login">Sign in</a>
+        </button>
+        <button type="submit">
+          <a href="register">Register</a>
+        </button>
+    </div>
     </>
   );
 }
