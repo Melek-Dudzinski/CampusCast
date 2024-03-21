45c02db953e82205b06cf72123dc2e6af439794e
Selin <s.djemal@se22.qmul.ac.uk>
2024-03-21 08:52:50 +0000

    Update sunRiseSet.js

--- app/home/sunRiseSet.js
+++ app/home/sunRiseSet.js
@@ -3,8 +3,10 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function SunRiseSet({locationSelected}) {
+  //sets a useState for the weather data to ensure fteching it with a useEffect does not cause issues
   const [weatherData, setWeatherData] = useState(null);
 
+  //fetches data with the fetchData method
   const fetchData = async () => {
   try {
       const MY_API_KEY = "47587e19f823f14e08d26b63b7a1f07d"
@@ -20,9 +22,12 @@ export default function SunRiseSet({locationSelected}) {
   useEffect(() => {
     fetchData();
   }, [locationSelected]);
-    
+  
+  //changes time from UNIX time (time counting seconds from 1970) to standard 12 hour time (with AM and PM)
   function formatTime(date) {
+    //fetches the hours in 24 hour time
     let hours = date.getHours();
+    //fetches the minutes since the hour has passed and also adds a 0 at the beginning if the minutes are less than 0
     let minutes = String(date.getMinutes()).padStart(2, "0")
     let period = ""
     if (hours < 12) {
@@ -45,6 +50,7 @@ export default function SunRiseSet({locationSelected}) {
     return formattedTime
   };
 
+  //Fetches the data for sunrise and sunset, and if it fails sets them as 'Loading...'
   let sunriseTime = weatherData ? formatTime(new Date(weatherData.sys.sunrise * 1000)) : "Loading...";
   let sunsetTime = weatherData ? formatTime(new Date(weatherData.sys.sunset * 1000)) : "Loading...";
   
